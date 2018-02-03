3ea5a479a415725d5cf3f07b42e4060f4daacc0f
samsamrubio122 <36020175+samsamrubio122@users.noreply.github.com>
2018-02-03 14:55:47 +0800

    Update index.php

--- index.php
+++ index.php
@@ -14,203 +14,203 @@
       </tr>
       <tr>
       	<td> The PT-REPEATER-NM-1CE</td>
-      	<td><img src="2.png"></td>
+      	<td><img src="2.PNG"></td>
       	<td>The PT-REPEATER-NM-1CE features a single Ethernet port that can connect a LAN backbone which can also support either six PRI connections to aggregate ISDN lines, or 24 synchronous/asynchronous ports.</td>
       </tr>
        <tr>
       	<td> The PT-ROUTER-NM-1AM card</td>
-      	<td><img src="3.png"></td>
+      	<td><img src="3.PNG"></td>
       	<td>The PT-ROUTER-NM-1AM card features dual RJ-11 connectors, which are used for basic telephone service connections. The WIC-1AM uses one port for connection to a standard telephone line, and the other port can be connected to a basic analog telephone for use when the modem is idle.</td>
       </tr>
        <tr>
       	<td> Router: 1841</td>
-      	<td><img src="5.png"></td>
+      	<td><img src="5.PNG"></td>
       	<td>The Cisco 1841 Integrated Services Router provides two fixed 10/100 (100BASE-TX) Ethernet ports, two integrated High-Speed WAN Interface Card (HWIC) slots that are compatible with WAN Interface Card (WICs) and Voice/WAN Interface Cards (VWICs), and one internal Advanced Integration Module (AIM) slot</td>
       </tr>
       <tr>
       	<td> Router: 2620XM</td>
-      	<td><img src="6.png"></td>
+      	<td><img src="6.PNG"></td>
       	<td>The Cisco 2620XM Multiservice Router provides a one-network module slot platform with one fixed 10/100 (100BASE-TX) Ethernet port, two integrated WAN Interface Card (WIC) slots, and one Advanced Integration Module (AIM) slot.</td>
       </tr>
       <tr>
       	<td> Router: 2621XM</td>
-      	<td><img src="7.png"></td>
+      	<td><img src="7.PNG"></td>
       	<td>The Cisco 2621XM Multiservice Router provides a one-network module slot platform with two fixed 10/100 (100BASE-TX) Ethernet ports, two integrated WAN Interface Card (WIC) slots, and one Advanced Integration Module (AIM) slot.
         The 2621XM supports the same modules that the 2620XM supports</td>
       </tr>
        <tr>
       	<td> Router: 2811</td>
-      	<td><img src="8.png"></td>
+      	<td><img src="8.PNG"></td>
       	<td>The Cisco 2811 Integrated Services Router provides one Enhanced Network-Module slot with two fixed 10/100 (100BASE-TX) Ethernet ports, four integrated High-Speed WAN Interface Card (HWIC) slots that are compatible with WAN Interface Card (WICs), Voice Interface Cards (VICs) and Voice/WAN Interface Cards (VWICs), and dual Advanced Integration Module (AIM) slots.
         </td>
       </tr>
        <tr>
       	<td> Router: Router-PT</td>
-      	<td><img src="9.png"></td>
+      	<td><img src="9.PNG"></td>
       	<td>The Router-PT generic router provides ten slots, one console port, and one auxiliary port.
         </td>
       </tr>
       <tr>
       	<td> Switch: 2950-24</td>
-      	<td><img src="10.png"></td>
+      	<td><img src="10.PNG"></td>
       	<td>The Cisco Catalyst 2950-24 is a member of the Cisco Catalyst 2950 series switch family. It is a standalone, fixed-configuration, managed 10/100 switch providing user connectivity for small- to mid-sized networks.
         It does not support add-in modules..
         </td>
       </tr>
       <tr>
       	<td> Switch: 2950T-24</td>
-      	<td><img src="img/11.png"></td>
+      	<td><img src="img/11.PNG"></td>
       	<td>TThe Cisco Catalyst 2950T-24 is a member of the Catalyst 2950 Series Intelligent Ethernet Switch family. It is a fixed-configuration, standalone switch that provides wire-speed Fast Ethernet and Gigabit Ethernet connectivity for mid-sized networks.
         It does not support add-in modules
         </td>
       </tr>
       <tr>
       	<td> Switch: 2960-24TT</td>
-      	<td><img src="img/12.png"></td>
+      	<td><img src="img/12.PNG"></td>
       	<td>The Cisco Catalyst 2960-24TT is a member of the Catalyst 2960 Series Intelligent Ethernet Switch family. It is a fixed-configuration, standalone switch that provides wire-speed Fast Ethernet and Gigabit Ethernet connectivity for mid-sized networks.
         </td>
       </tr>
       <tr>
       	<td> Switch: Switch-PT</td>
-      	<td><img src="img/13.png"></td>
+      	<td><img src="img/13.PNG"></td>
       	<td>The Switch-PT generic switch provides ten slots, one console port, and one auxiliary port.
         </td>
       </tr>
       <tr>
       	<td> PC-PT</td>
-      	<td><img src="img/14.png"></td>
+      	<td><img src="img/14.PNG"></td>
       	<td>The PC-PT provides a console port and one slot.
         </td>
       </tr>
       <tr>
       	<td> Laptop-PT</td>
-      	<td><img src="img/15.png"></td>
+      	<td><img src="img/15.PNG"></td>
       	<td>The Laptop-PT provides a console port and one slot.
         </td>
       </tr>
       <tr>
       	<td> Server-PT</td>
-      	<td><img src="img/16.png"></td>
+      	<td><img src="img/16.PNG"></td>
       	<td>The Server-PT provides one slot.
         The Server-PT supports the same modules as the PC-PT except for the PC-HOST-NM-1AM module.
         </td>
       </tr>
        <tr>
       	<td> Printer-PT</td>
-      	<td><img src="img/17.png"></td>
+      	<td><img src="img/17.PNG"></td>
       	<td>The Printer-PT provides one slot.
        The Printer-PT supports the same modules as the PC-PT except for the PC-HOST-NM-1AM module.
         </td>
       </tr>
       <tr>
         <td> 7960</td>
-        <td><img src="img/18.png"></td>
+        <td><img src="img/18.PNG"></td>
         <td>The 7960 IP Phone only provides a port for the power adapter.
         </td>
       </tr>
        <tr>
         <td> Home-VoIP-PT</td>
-        <td><img src="img/19.png"></td>
+        <td><img src="img/19.PNG"></td>
         <td>The Home-VoIP-PT does not support modules.
         </td>
       </tr>
       <tr>
         <td>Analog-Phone-PT</td>
-        <td><img src="img/20.png"></td>
+        <td><img src="img/20.PNG"></td>
         <td>The Analog-Phone-PT does not support modules.
         </td>
       </tr>
        <tr>
         <td>TV-PT</td>
-        <td><img src="img/21.png"></td>
+        <td><img src="img/21.PNG"></td>
         <td>The TV-PT does not support modules.
         </td>
       </tr>
       <tr>
         <td>WirelessEndDevice-PT</td>
-        <td><img src="img/22.png"></td>
+        <td><img src="img/22.PNG"></td>
         <td>The WirelessEndDevice-PT does not support modules. However, it has a built-in Wireless interface.
         </td>
       </tr>
       <tr>
         <td>WiredDevice-PT</td>
-        <td><img src="img/23.png"></td>
+        <td><img src="img/23.PNG"></td>
         <td>The WiredDevice-PT does not support modules. However, it has a built-in FastEthernet interface.
         </td>
       </tr>
       <tr>
         <td>Hub-PT</td>
-        <td><img src="img/24.png"></td>
+        <td><img src="img/24.PNG"></td>
         <td>The Hub-PT provides ten slots.
         </td>
       </tr>
       <tr>
         <td>Repeater-PT</td>
-        <td><img src="img/24.png"></td>
+        <td><img src="img/24.PNG"></td>
         <td>The Repeater-PT provides two slots.
          The Repeater-PT supports the same modules that the Hub-PT supports.
         </td>
       </tr>
       <tr>
         <td>CoAxialSplitter-PT</td>
-        <td><img src="img/25.png"></td>
+        <td><img src="img/25.PNG"></td>
         <td>The CoAxialSplitter-PT does not support modules.
         </td>
       </tr>
       <tr>
         <td>AccessPoint-PT</td>
-        <td><img src="img/26.png"></td>
+        <td><img src="img/26.PNG"></td>
         <td>The AccessPoint-PT has a built-in antenna and provides one slot.
         The AccessPoint-PT supports the same modules that the Hub-PT supports.
         </td>
       </tr>
        <tr>
         <td>Linksys-WRT300N</td>
-        <td><img src="img/27.png"></td>
+        <td><img src="img/27.PNG"></td>
         <td>The Linksys-WRT300N wireless router has a built-in antenna and provides four fixed 10/100 (100BASE-TX) Ethernet ports and one fixed Internet port (also 100BASE-TX, typically for connection to cable and DSL modems).
         The Linksys-WRT300N wireless router does not support modules.
         </td>
       </tr>
        <tr>
         <td>Cloud-PT</td>
-        <td><img src="img/28.png"></td>
+        <td><img src="img/28.PNG"></td>
         <td>Although a cloud is not a single device, Packet Tracer gives you access to a representation of a cloud. It provides ten slots, a console port, and an auxiliary port.
         </td>
       </tr>
       <tr>
         <td>DSL-Modem-PT</td>
-        <td><img src="img/29.png"></td>
+        <td><img src="img/29.PNG"></td>
         <td>The DSL-Modem-PT provides one slot.
         </td>
       </tr>
        <tr>
         <td>Cable-Modem-PT</td>
-        <td><img src="img/30.png"></td>
+        <td><img src="img/30.PNG"></td>
         <td>The Cable-Modem-PT provides one slot.
         The Cable-Modem-PT supports the same modules that the DSL-Modem-PT supports.
         </td>
       </tr>
        <tr>
         <td>Unshielded Twisted Pair Cable</td>
-        <td><img src="img/31.png"></td>
+        <td><img src="img/31.PNG"></td>
         <td>Unshielded Twisted Pair Cable (UTP) is the most common type of cable used in networks. Almost all Ethernet LANs are built using UTP cables. UTP cables are thin and flexible and very cost effective which makes them the ideal choice for Ethernet cabling. The EIA/TIA (Electronic Industry Association/Telecommunication Industry Association) has established standards of UTP and rated various categories of wire some of which are shown below.
         </td>
       </tr>
       <tr>
         <td> Shielded Twisted Pair Cable (STP) </td>
-        <td><img src="img/32.png"></td>
+        <td><img src="img/32.PNG"></td>
         <td> Shielded Twisted Pair Cable (STP) wraps each pair of wire in a metallic foil and further wraps all four pairs if wires in a metallic braid or foil, this further reduces the noise both within the cable from outside the cable. STP cable is more expensive then UTP cable and is much more difficult to install and manage. It also requires grounding at both ends of the metallic shield.
         </td>
       </tr>
       <tr>
         <td>Coxial cable </td>
-        <td><img src="img/34.png"></td>
+        <td><img src="img/34.PNG"></td>
         <td> Coxial cable is the most popular cable in cable television industry, a coaxial cable consists of copper conductor which is surrounded by an insulation material, over the insulation material is copper braid or foil that acts both as the second wire in the circuit and as a shield for the inner conductor thereby protecting it from interference. The coaxial cable is difficult to install and manage but it can support much greater cable lengths.
         </td>
       </tr>
        <tr>
         <td>Fiber-Optic cable </td>
-        <td><img src="img/35.png"></td>
+        <td><img src="img/35.PNG"></td>
         <td>Fiber-Optic cable is made up of glass and plastic fibers surrounded by several layers of protective materials. Fiber-optic cables are much more expensive than twisted-pair and coaxial cables. Fiber-optic cables are immune to EMI and can run small and large distances depending upon the type of cable. Fiber-optic cables support very high bandwidth which makes them the preferred choice of cables when high speed connectivity is required.
         
 
